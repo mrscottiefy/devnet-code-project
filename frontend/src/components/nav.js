b1f668e5d9f0c188a7c655761023598a63f6cc99
mrscottiefy <scottnathaniel.lim@gmail.com>
2021-08-21 20:19:39 +0800

    .

---
+++ frontend/src/components/nav.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../App.css';
+import {Link} from 'react-router-dom';
+
+function Nav() {
+
+    const newStyle = {
+        color: 'white'
+    }
+
+    return (
+        <nav>
+            <Link to="/" style={newStyle}>
+                <h3>Logo</h3>
+            </Link>
+            <ul className="nav-links">
+                <Link to="/about" style={newStyle}>
+                    <li>About</li>
+                </Link>
+                <Link to="/shop" style={newStyle}>
+                    <li>Shop</li>
+                </Link>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
